9062402c6de96cbb2edb61c7fdc3a4eb4496b41d
Oyeyipo Emmanuel <emmasco0207@gmail.com>
2023-09-07 02:32:19 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const date = new Date
+const utcTime = date.getUTCMilliseconds()
+const utcTimeElement = document.querySelector('.utcTime');
+utcTimeElement.innerHTML = "UTC Time in milli seconds: " + utcTime;
+const dayVal = date.getDay() + 1;
+
+const dayArray = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const dayOfWeekElement = document.querySelector('.dayOfWeek');
+dayOfWeekElement.innerHTML = "Today is : " + dayArray[dayVal];
+console.log(dayArray[dayVal])
