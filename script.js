bd1ca34a6b1881753c1a36332e15f2942c701e7e
OyeyipoEmmanuel <113785444+OyeyipoEmmanuel@users.noreply.github.com>
2023-09-08 13:22:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const date = new Date
-const utcTime = date.getTime()
-const utcTimeElement = document.querySelector('.utcTime');
-utcTimeElement.innerHTML = "UTC Time in milli seconds: " + utcTime;
-const dayVal = date.toLocaleDateString('en-US', { weekday: 'long' });
-const dayOfWeekElement = document.querySelector('.dayOfWeek');
-dayOfWeekElement.innerHTML = "Today is : " + dayVal;
+
