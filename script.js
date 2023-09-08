97012302c17c1f37489c8cfc648434acee26ca16
OyeyipoEmmanuel <113785444+OyeyipoEmmanuel@users.noreply.github.com>
2023-09-08 11:11:26 +0100

    Changed the day code

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const date = new Date
 const utcTime = date.getTime()
 const utcTimeElement = document.querySelector('.utcTime');
 utcTimeElement.innerHTML = "UTC Time in milli seconds: " + utcTime;
-const dayVal = date.getDay() + 1;
-
-const dayArray = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+const dayVal = date.toLocaleDateString('en-US', { weekday: 'long' });
 const dayOfWeekElement = document.querySelector('.dayOfWeek');
-dayOfWeekElement.innerHTML = "Today is : " + dayArray[dayVal];
-console.log(dayArray[dayVal])
+dayOfWeekElement.innerHTML = "Today is : " + dayVal;
